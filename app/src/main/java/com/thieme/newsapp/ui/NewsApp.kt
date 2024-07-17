cf3f351b307348655f85c4becd1febba948a256d
Josh Thieme <jomthieme@gmail.com>
2024-07-16 22:48:50 -0600

    got the bottom navigation implemented and working

--- app/src/main/java/com/thieme/newsapp/ui/NewsApp.kt
+++ app/src/main/java/com/thieme/newsapp/ui/NewsApp.kt
@@ -1,26 +1,46 @@
 package com.thieme.newsapp.ui
 
+import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import com.thieme.newsapp.BottomMenuScreen
 import com.thieme.newsapp.MockData
+import com.thieme.newsapp.ui.screen.Categories
 import com.thieme.newsapp.ui.screen.DetailScreen
+import com.thieme.newsapp.ui.screen.Sources
 import com.thieme.newsapp.ui.screen.TopNews
+import com.thieme.newsapp.components.BottomMenu
 
 @Composable
 fun NewsApp() {
-    Navigation()
+    val scrollState = rememberScrollState()
+    val navController = rememberNavController()
+    MainScreen(navController = navController, scrollState = scrollState)
 }
 
 @Composable
-fun Navigation() {
-    val navController = rememberNavController()
-    val scrollState = rememberScrollState()
+fun MainScreen(navController: NavHostController, scrollState: ScrollState) {
+    Scaffold(bottomBar = {
+        BottomMenu(navController)
+    }) {
+        Navigation(navController = navController, scrollState = scrollState)
+    }
+
+}
+
+@Composable
+fun Navigation(navController: NavHostController, scrollState: ScrollState) {
     NavHost(navController = navController, startDestination = "TopNews") {
+        bottomNavigation(navController = navController)
         composable("TopNews") {
             TopNews(navController = navController)
         }
@@ -30,5 +50,17 @@ fun Navigation() {
             val newsData = MockData.getNewsById(id)
             DetailScreen(newsData, scrollState, navController)
         }
+    }
+}
+
+fun NavGraphBuilder.bottomNavigation(navController: NavController) {
+    composable(BottomMenuScreen.TopNews.route) {
+        TopNews(navController = navController)
+    }
+    composable(BottomMenuScreen.Categories.route) {
+        Categories()
+    }
+    composable(BottomMenuScreen.Sources.route) {
+        Sources()
     }
 }
---
+++ app/src/main/java/com/thieme/newsapp/ui/screen/Categories.kt
@@ -0,0 +1,10 @@
+package com.thieme.newsapp.ui.screen
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import com.thieme.newsapp.BottomMenuScreen
+
+@Composable
+fun Categories() {
+    Text(text = "Categories")
+}
